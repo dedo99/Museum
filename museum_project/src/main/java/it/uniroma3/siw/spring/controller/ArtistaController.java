603f471d6d89cfedc17777917c75314ec0717872
AjejeBrazorf99 <72614834+AjejeBrazorf99@users.noreply.github.com>
2021-05-25 18:35:53 +0200

    collezioni e artisti

--- museum_project/src/main/java/it/uniroma3/siw/spring/controller/ArtistaController.java
+++ museum_project/src/main/java/it/uniroma3/siw/spring/controller/ArtistaController.java
@@ -3,6 +3,7 @@ package it.uniroma3.siw.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,5 +20,12 @@ public class ArtistaController {
 		model.addAttribute("artisti", this.artistaService.tuttiArtisti());
 		return "artisti.html";
 	}
+	
+	@RequestMapping(value = "/artista/{nome}", method = RequestMethod.GET) 
+	public String getArtista(@PathVariable("nome") String nome, Model model) {
+		model.addAttribute("artista",this.artistaService.artistaPerId(nome));
+		return "artista.html";
+	}
+	
 
 }
--- museum_project/src/main/java/it/uniroma3/siw/spring/model/Opera.java
+++ museum_project/src/main/java/it/uniroma3/siw/spring/model/Opera.java
@@ -4,7 +4,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import lombok.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
 
 @Entity
